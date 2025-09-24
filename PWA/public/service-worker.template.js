f6e0a1ed27f419a1f8ef4763f6d26f2741dca46a
larsnygard <67096522+larsnygard@users.noreply.github.com>
2025-09-24 20:48:06 +0200

    Create service-worker.template.js for PWA
    Add a service worker template for caching and fetch handling.

---
+++ PWA/public/service-worker.template.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("snartnet-cache").then(cache => {
+      return cache.addAll(["{{BASE_PATH}}", "{{BASE_PATH}}index.html"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
